6c737609ade15a327c56a1e216e251530c351d51
Takehiro Iyatomi <iyatomi@gmail.com>
2014-07-15 21:25:10 +0900

    add missing package and fix benchmark install script

--- test/tools/bench_install.sh
+++ test/tools/bench_install.sh
@@ -5,16 +5,18 @@ git clone https://github.com/methane/echoserver.git /tmp/echoserver
 
 # create package dir
 mkdir -p /tmp/packages/
+pushd /tmp/packages
 
 # install go
-wget http://golang.org/dl/go1.3.linux-amd64.tar.gz /tmp/packages/go.tar.gz
-tar -C /usr/local -xzf /tmp/packages/go.tar.gz
+GO_VERSION=1.3.linux-amd64
+wget http://golang.org/dl/go$GO_VERSION.tar.gz
+tar -C /usr/local -xzf go$GO_VERSION.tar.gz
 
 # install node
-wget http://nodejs.org/dist/v0.10.29/node-v0.10.29.tar.gz /tmp/packages/node.tar.gz
-pushd /tmp/packages
-tar -zxf node.tar.gz
-pushd node-v0.10.29
+NODE_VERSION=v0.10.29
+wget http://nodejs.org/dist/v0.10.29/node-$NODE_VERSION.tar.gz
+tar -zxf node-$NODE_VERSION.tar.gz
+pushd node-$NODE_VERSION
 ./configure && make && make install
 popd
 popd
