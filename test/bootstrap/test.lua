a2e214e7307dd5448fc5376a40a21d4209b5b596
Takehiro Iyatomi <iyatomi@gmail.com>
2015-03-13 12:09:51 +0900

    try enable luajit 2.1.0 again

--- test/bootstrap/test.lua
+++ test/bootstrap/test.lua
@@ -1,13 +1,27 @@
 local dir = io.popen('ls test')
 local term = require 'pulpo.terminal'
 
+
+local idx = -1
+while true do
+	if arg[idx]:match('^luajit') or arg[idx]:match('^lj') then
+		break
+	elseif arg[idx] == '-e' then
+		-- quote and escape lua code
+		arg[idx + 1] = ('"%s"'):format(arg[idx + 1]:gsub('"', '\\"'))
+	end
+	idx = idx - 1
+end	
+cmdl = table.concat({unpack(arg, idx, -1)}, ' ')
+print('cmdl', cmdl)
+
 while true do
 	local file = dir:read()
 	if not file then break end
 	file = ('test/' .. file)
 	if file:find('%.lua$') then
 		term.resetcolor(); print('test: '..file..' ==========================================')
-		local ok, r = pcall(os.execute, arg[-1].." test/tools/launch.lua "..file.." "..table.concat(arg, " "))
+		local ok, r = pcall(os.execute, cmdl.." test/tools/launch.lua "..file.." "..table.concat(arg, " "))
 		if ok and r then
 			if r ~= 0 then
 				term.red(); print('test fails:' .. file .. '|' .. r)
